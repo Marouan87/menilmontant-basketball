396510ad605334bce1f31856164c8fe0b051f4d5
Marouan El Mekawy <marouan.elmekawy@gmail.com>
2026-09-16 14:35:49 +0200

    Utilise le logo blanc detoure dans l'en-tete et le pied de page

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -9,7 +9,7 @@ export default function SiteFooter() {
         <div className="footer-grid">
           <div>
             <Image
-              src="/images/logo-navy.png"
+              src="/images/logo-white.png"
               alt=""
               width={62}
               height={62}
